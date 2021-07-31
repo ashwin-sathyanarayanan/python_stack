1aeebe4c58c9dfd98f0dab04187c6f312c008b2c
Ashwin Sathyanarayanan <ashwin.sathyanarayanan@cgius.com>
2021-07-31 01:39:59 -0700

    completed Users and templates assignment

--- django/django_orm/single_model_orm/users_app/views.py
+++ django/django_orm/single_model_orm/users_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
 
 # Create your views here.
+def index(request):
+    context = {
+        'all_users': User.objects.all()
+    }
+    return render(request,'index.html', context)
+
+def create_user(request):
+    new_user = User.objects.create(first_name= request.POST['first_name'], last_name =request.POST['last_name'], email_address = request.POST['email_address'] , age = request.POST['age'])
+    return redirect('/')
